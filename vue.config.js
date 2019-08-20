e9bd3be8b50dcba0a57f6b75a62b48abee0e2b85
zhengshaowen <zhengshaowen@zuoyebang.com>
2019-08-20 15:51:59 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,11 @@
  * @Date: 2019-08-20 14:54:36
  * @Author: zhengshaowen
  * @LastEditors: zhengshaowen
- * @LastEditTime: 2019-08-20 14:56:53
+ * @LastEditTime: 2019-08-20 15:48:32
  */
 /* eslint-disable*/
 module.exports = {
   productionSourceMap: false,
-  publicPath: "./test-pwa"
+  publicPath: "./test-pwa",
+  assetsDir: "static"
 }
